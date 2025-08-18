dc04d45d2aecb522f954bb601f8bd260f12a30b4
Git-da-K <k.bruna.schmidt.vieira@gmail.com>
2025-08-18 00:18:58 -0300

    Update bot_logic.py

--- bot_logic.py
+++ bot_logic.py
@@ -19,7 +19,8 @@ def gen_emodji():
 def flip_coin():
     flip = random.randint(0, 2)
     if flip == 0:
-        return "ОРЕЛ"
+        return "cara"
     else:
-        return "РЕШКА"
+        return "coroa"
+
  
